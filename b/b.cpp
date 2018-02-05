a76c16662ec3ad3182c71b1cc6869d2e1ac07b60
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-02-05 11:21:30 +0300

    CMake code

---
+++ b/b.cpp
@@ -0,0 +1,12 @@
+#include <b/b.hpp>
+
+#include <x/x.hpp>
+
+namespace b {
+
+void b::Run() {
+  x::x var;
+  var.Run();
+}
+
+} // namespace a
---
+++ b/b.hpp
@@ -0,0 +1,13 @@
+#ifndef B_B_HPP_
+#define B_B_HPP_
+
+namespace b {
+
+class b {
+ public:
+  void Run();
+};
+
+} // namespace b
+
+#endif // B_B_HPP_
